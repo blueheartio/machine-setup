2f6248e7f4e72ad5a18a5102dddba8ae5e1e707e
Robbie <robbie.coomber@gmail.com>
2021-08-25 14:32:04 +0100

    Add mac setup script

---
+++ bootstrap-mac.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+########
+# bootstrap-mac.sh
+#
+# The purpose of this script is to set up a development environment. It is designed to be idempotent, so if you
+# already have a functioning setup, it should do nothing
+
+# Xcode
+if ! [ -x "$(command -v xcode-select)" ]; then
+  echo "Couldn't find xcode-select! Please make sure you are on a mac with Xcode installed" >&2
+  exit 1
+fi
+xcode-select --install # does nothing if the tools are already installed
+
+# Homebrew
+if ! [ -x "$(command -v brew)" ]; then
+  echo "Homebrew does not exist, installing..."
+  # see https://docs.brew.sh/Installation for help
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit 1
+else
+  echo "Homebrew already exists, skipping"
+fi
+
+# Node
+if ! [ -x "$(command -v node)" ]; then
+  echo "Node does not exist, installing..."
+  brew install node@12 || exit 1
+else
+  echo "Node already exists, skipping"
+fi
+
+# Docker for Mac
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Docker does not exist, installing..."
+  brew install homebrew/cask/docker || exit 1
+else
+  echo "Docker already exists, skipping"
+fi
+
+# Postgres
+if ! [ -x "$(command -v psql)" ]; then
+  echo "Postgres does not exist, installing..."
+  brew install postgres || exit 1
+else
+  echo "Postgres already exists, skipping"
+fi
+
+# Project directory
+mkdir -p ~/Projects
+cd ~/Projects
+
+# Repo
+if ! [[ -d "~/Projects/blueheart/.git" ]]
+then
+    echo "A repo already exists at ~/Projects/blueheart/ so skipping git clone"
+else
+  git clone git@github.com:blueheartio/blueheart.git || exit 1
+fi
+cd blueheart
+
+# Yarn install
+yarn install || exit 1
+yarn tsc || exit 1
+
+# Done!
+echo "✨✨✨"
+echo "Dependencies are installed!"
+echo "cd ~/Projects/blueheart"
+echo "Please check the README.md to see what you can do next"
